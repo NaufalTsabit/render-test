46a33b152e6c154e8aecd54d2e9dacb31dd1e9de
NaufalTsabit <naufaltsabit10@gmail.com>
2025-12-16 11:10:24 +0700

    Add Frontend

--- index.js
+++ index.js
@@ -35,6 +35,7 @@ const generateId = () => {
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static('dist'))
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World</h1>')
